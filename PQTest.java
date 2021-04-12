99daf3f3a147feb80ed54914e96b3931c117d6a1
KimDongGyeom <79586269+KimDongGyeom@users.noreply.github.com>
2021-04-12 15:48:13 +0900

    Add files via upload

---
+++ PQTest.java
@@ -0,0 +1,28 @@
+package dddd;
+
+import java.util.*;
+
+public class PQTest {
+	public static void main(String[] args) {
+		test1();
+	}
+	
+	public static void test1() {
+		Queue<String> q = new PriorityQueue<>();
+		
+		q.offer("PineApple"); // q.add();
+		q.offer("Banana");
+		q.offer("Carrot");
+		q.offer("Cherry");
+		q.offer("Strawberry");
+		
+		// 엿보기. 인출은 아니요
+		System.out.println(q.peek()); // q.element()
+		System.out.println(q.peek());
+		System.out.println(q.peek());
+		System.out.println("##############");
+		while(q.size() > 0) {
+			System.out.println(q.poll()); // q.remove()
+		}
+	}
+}
